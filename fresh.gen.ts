8ca9826113ad216f13902ad737798a53c7db84f2
mohammed <mohammedsebbagh@gmail.com>
2022-11-12 20:46:15 -0500

    Docs Route almost done

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,13 +4,15 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/about.tsx";
-import * as $1 from "./routes/index.tsx";
+import * as $1 from "./routes/docs.tsx";
+import * as $2 from "./routes/index.tsx";
 import * as $$0 from "./islands/MemoryChart.tsx";
 
 const manifest = {
   routes: {
     "./routes/about.tsx": $0,
-    "./routes/index.tsx": $1,
+    "./routes/docs.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {
     "./islands/MemoryChart.tsx": $$0,
