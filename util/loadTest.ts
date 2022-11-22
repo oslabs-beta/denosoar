c65a807544c30131d3956d5396a7ea5c9648eb15
Ethan Liu <eliu080893@gmail.com>
2022-11-21 20:02:26 -0500

    CLI siege incorporated

--- util/loadTest.ts
+++ util/loadTest.ts
@@ -1,3 +1,40 @@
-export default function hi(echo: string){
-  console.log(echo);
+import { assert } from "https://deno.land/std/testing/asserts.ts";
+
+export default function loadtest(url: string, concurrency: string, rps: string, duration:string) {
+  // const array = new Array(10).fill(url);
+  // console.log(array);
+
+  const numbered = Number(concurrency);
+  const n = 1000;
+
+  function getRequests(url: string, concurrency: number): any[] {
+    // console.log('L13 : now Invoking getRequests');
+    return new Array(concurrency).fill(
+      new Promise((resolve) => {
+        const res = fetch(url);
+        return resolve(res);
+      }),
+    );
+  }
+
+  const siegeInterval = setInterval(async () => {
+    const response = await Promise.all(getRequests(url, Number(concurrency)));
+    console.log(response);
+  }, n / Number(rps));
+
+  setTimeout( ()=> clearInterval(siegeInterval), Number(duration))
+
+  // Deno.test("Load test", async () => {
+  //   let status200 = 0, statusNon200 = 0;
+  //   const totalRequests = 50000;
+  //   for (let i = 0; i < 50; i++) {
+  //     const responses = await Promise.all(getRequests("https://deno.land"));
+  //     for (const s of responses) {
+  //       s === 200 ? status200++ : statusNon200++;
+  //     }
+  //   }
+  //   assert(status200 === totalRequests);
+  // });
+
+
 }
